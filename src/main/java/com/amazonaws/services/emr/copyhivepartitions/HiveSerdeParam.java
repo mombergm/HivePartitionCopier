3775abbf12b3a7e8872a25216b750bd194495efe
Mandus Momberg <mombergm@amazon.com>
2015-01-25 21:03:09 +0000

    Added String Escaper

--- src/main/java/com/amazonaws/services/emr/copyhivepartitions/HiveSerdeParam.java
+++ src/main/java/com/amazonaws/services/emr/copyhivepartitions/HiveSerdeParam.java
@@ -29,8 +29,24 @@ public class HiveSerdeParam {
     if(!isFirst){
       sqlout+= ",";
     }
-    sqlout += "(" +SERDE_ID+ ", '" +PARAM_KEY+ "', '"+PARAM_VALUE+"')"; 
+    sqlout += "(" + SERDE_ID + ", '" +PARAM_KEY+ "', '"+ getMysqlRealScapeString( PARAM_VALUE )+"')"; 
     return sqlout; 
   }
+  
+  public static synchronized String getMysqlRealScapeString(String str) {
+	  String data = null;
+	  if (str != null && str.length() > 0) {
+	   str = str.replace("\\", "\\\\");
+	   str = str.replace("'", "\\'");
+	   str = str.replace("\0", "\\0");
+	   str = str.replace("\n", "\\n");
+	   str = str.replace("\r", "\\r");
+	   str = str.replace("\"", "\\\"");
+	   str = str.replace("\\x1a", "\\Z");
+	   data = str;
+	  }
+	  return data;
+	 }
 
 }
+
